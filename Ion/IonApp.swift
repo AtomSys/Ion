a5acb25d047ff0165a05c2c9c62fec7eeae6d876
AtomSys <zcrenshaw1@icloud.com>
2023-04-22 10:24:55 -0400

    Initial Commit

---
+++ Ion/IonApp.swift
@@ -0,0 +1,17 @@
+//
+//  IonApp.swift
+//  Ion
+//
+//  Created by Zeb Crenshaw on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct IonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
